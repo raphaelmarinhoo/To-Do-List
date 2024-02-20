7fa516a220a5f302e8741c591250025efae72dad
Raphael <raphael.lukas.m@gmail.com>
2024-02-19 23:53:19 -0300

    add: bottons funcionality

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,18 +1,25 @@
-import React from 'react'
+import React from "react";
 
-const Todo = ({todo}) => {
-    return (
-        <div className="todo">
-            <div className="content">
-                <p>{todo.text}</p>
-                <p className="category">({todo.category})</p>
-            </div>
-            <div>
-                <button className="complete">Completar</button>
-                <button className="remove">X</button>
-            </div>
-        </div>
-    )
-}
+const Todo = ({ todo, removeTodo, completeTodo }) => {
+  return (
+    <div
+      className="todo"
+      style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}
+    >
+      <div className="content">
+        <p>{todo.text}</p>
+        <p className="category">({todo.category})</p>
+      </div>
+      <div>
+        <button className="complete" onClick={() => completeTodo(todo.id)}>
+          Completar
+        </button>
+        <button className="remove" onClick={() => removeTodo(todo.id)}>
+          X
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default Todo
+export default Todo;
--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,15 +1,15 @@
 import { useState } from "react";
 
-const TodoForm = ({addTodo}) => {
+const TodoForm = ({ addTodo }) => {
   const [value, setValue] = useState("");
   const [category, setCategory] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if(!value || !category) return;
-    addTodo(value, category)
-    setValue("")
-    setCategory("")
+    if (!value || !category) return;
+    addTodo(value, category);
+    setValue("");
+    setCategory("");
   };
   return (
     <div className="todo-form">
